f03a8f700c55877b5a46cbc7b8db64807b101d20
sameet <sameetmoosavi@yahoo.com>
2022-07-14 16:42:50 +0430

    update layouts (adding header and remove div>.container

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,18 @@
+import Head from 'next/head'
 import styles from '../styles/Layout.module.css'
 
 const Layout = ({children}) => {
   return (
-    <main className={styles.main}>
-      {children}
-    </main>
+    <>
+      <Head>
+        <title>Sameet@Moosavi</title>
+        <meta name='description' content='sameetmoosavi website' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main className={`w-3/5 container ${styles.main}`}>
+        {children}
+      </main>
+    </>
   )
 }
 
