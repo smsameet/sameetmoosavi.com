0b002eb1e89adf15580fcd49485abf5f584681b7
sameet <sameetmoosavi@yahoo.com>
2022-07-15 15:05:51 +0430

    update index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,8 @@
 const index = () => {
   return (
     <>
-      <div className="bg-slate-700 text-white text-lg text-center p-3 rounded-xl mt-20 sm:w-6/6">
-        <span>Hello I&apos;m full-stack and software developer based in Iran!</span>
+      <div className='bg-slate-700 text-white text-center rounded-xl mt-10 md:pl-20 md:pr-20 p-3 w-fit m-auto'>
+        <span className=''>Hello I&apos;m full-stack and software developer based in Iran!</span>
       </div>
     </>
   )
