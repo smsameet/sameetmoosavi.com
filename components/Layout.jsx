34f3c410c4ea03b9e61320a816aa2220bb408d48
sameet <sameetmoosavi@yahoo.com>
2022-07-16 21:20:32 +0430

    update layouts (remove classnames and navbar)

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,16 +1,14 @@
 import Head from 'next/head'
-import Navbar from '../components/Navbar'
 
 const Layout = ({children}) => {
   return (
-    <main className='flex flex-col p-3 pb-8 m-auto container md:w-3/6'>
+    <main>
       <Head>
         <title>Sameet@Moosavi</title>
         <meta name='description' content='sameetmoosavi website' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Navbar />
-      <div className='pt-14'>
+      <div>
         {children}
       </div>
     </main>
