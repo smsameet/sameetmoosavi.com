770dd49438bac4e6ce67ccfbc225fddbf83db3dd
sameet <sameetmoosavi@yahoo.com>
2022-07-19 15:25:02 +0430

    update index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,9 @@
 const index = () => {
   return (
     <>
-      <div className='flex justify-center m-10'><img className='' src='/favicon.ico' width='200px' height='200px'></img></div>
+      <div className='flex justify-center m-10'><img className='border-2 border-white rounded-full shadow-lg shadow-black' src='/favicon.ico' width='200px' height='200px'></img></div>
       <div className='flex m-auto mt-5 justify-center'>
-        <h2 className='text-white p-3 bg-gray-600 rounded-md'>Hello I&apos;m full-stack and software developer based in Iran!</h2>
+        <h2 className='text-white p-3 bg-gray-600 rounded-md shadow-lg shadow-black'>Hello I&apos;m full-stack and software developer based in Iran!</h2>
       </div>
     </>
   )
