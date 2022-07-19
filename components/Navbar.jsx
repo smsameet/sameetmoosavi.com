cda45fa51afd76144d1e55940ba955b631725af6
sameet <sameetmoosavi@yahoo.com>
2022-07-19 15:24:50 +0430

    update navigation bar classes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <>
       <div className='flex items-center'>
-        <Link href='/'><a><span className='font-bold text-white text-lg'>Sameet</span><span className='text-red-600 text-lg'>@</span><span className='font-bold text-white text-lg'>Moosavi</span></a></Link>
+        <Link href='/'><a><span className='text-blue-600 font-bold text-2xl'>[ </span><span className='font-bold text-white text-lg'>Sameet</span><span className='text-red-600 text-lg'>@</span><span className='font-bold text-white text-lg'>Moosavi</span><span className='text-blue-600 font-bold text-2xl'> ]</span></a></Link>
         <div className='ml-auto md:hidden'>
           <div className="relative inline-block text-left">
             <div>
@@ -20,7 +20,7 @@ const Navbar = () => {
               </button>
             </div>
             {showOptions && (
-             <div className="origin-top-right text-center absolute right-2 mt-3 w-80 rounded-md bg-white" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+             <div className="origin-top-right text-center absolute right-0 mt-3 w-52 rounded-md bg-white" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                 <div className="py-1" role="none">
                   <Link href='/works' target='_blank'><a className="text-gray-800 block px-4 py-2 text-md" role="menuitem" tabindex="-1" id="menu-item-1">Works</a></Link>
                   <Link href='/posts' target='_blank'><a className="text-gray-800 block px-4 py-2 text-md" role="menuitem" tabindex="-1" id="menu-item-1">Posts</a></Link>
@@ -30,8 +30,8 @@ const Navbar = () => {
           </div>
         </div>
         <div className='ml-9 md:flex hidden'>
-          <Link href='/works' target='_blank'><a className="text-white hover:text-yellow-500 px-4 py-2 text-md" role="menuitem" tabindex="-1" id="menu-item-1">Works</a></Link>
-          <Link href='/posts' target='_blank'><a className="text-white hover:text-yellow-500 px-4 py-2 text-md" role="menuitem" tabindex="-1" id="menu-item-1">Posts</a></Link>
+          <Link href='/works' target='_blank'><a className="text-white hover:text-yellow-500 px-2 py-2 text-md" role="menuitem" tabindex="-1" id="menu-item-1">Works</a></Link>
+          <Link href='/posts' target='_blank'><a className="text-white hover:text-yellow-500 px-2 py-2 text-md" role="menuitem" tabindex="-1" id="menu-item-1">Posts</a></Link>
         </div>
       </div>
     </>
