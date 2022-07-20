b9b245146318693e04fecc55cd4584a75dd6bd4e
sameet <sameetmoosavi@yahoo.com>
2022-07-20 17:14:18 +0430

    update layouts template

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({children}) => {
         <meta name='description' content='sameetmoosavi website' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div className='container md:w-3/6 m-auto p-4'>
+      <div className='container md:w-5/12 m-auto p-4 mb-10'>
         {children}
       </div>
     </main>
